324da00bd890db4175ebb0c21334ed0fdf029414
root <jackmon22523@gmail.com>
2022-09-09 11:34:27 -0700

    drive me nuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+
 /**
- * *main-Entry point
  *
- * Reurn:Always 0 (SUcces)
- */
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
-	puts("|"Programming is like building a multilingual puzzle");
-	return (0)
-}
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
