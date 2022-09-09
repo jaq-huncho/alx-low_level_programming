2b5e4365c839cea0f4186eeba3bd22f2b3334c83
root <jackmon22523@gmail.com>
2022-09-09 09:57:07 -0700

    master

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * *main-Entry point
+ *
+ * Reurn:Always 0 (SUcces)
+ */
+int main(void)
+{
+	puts("|"Programming is like building a multilingual puzzle");
+	return (0)
+}
