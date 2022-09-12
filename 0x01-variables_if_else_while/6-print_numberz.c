dd699c3a63adf067b812b802a4879105d5a13839
root <jackmon22523@gmail.com>
2022-09-12 12:09:25 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,12 @@ int main(void)
 {
 	int s;
 
-	for (s = 0; s <= 10; s++)
-	{
-		putchar(s + '0');
-		s++;
-	}
+	while
+		(s < 10)
+		{
+			putchar(s + '0');
+			s++;
+		}
 	putchar('\n');
 	return (0);
 }
