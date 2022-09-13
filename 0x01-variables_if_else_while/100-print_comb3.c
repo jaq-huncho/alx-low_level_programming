243652a24a37a42fda58fc3273b1c6d96f9018ed
root <jackmon22523@gmail.com>
2022-09-12 18:12:56 -0700

    assign

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, c;
+
+	for (a = 1; a < 9; a++)
+	{
+		for (c = a + 1; c < 10; c++)
+		{
+			putchar((a % 10) + '0');
+			putchar((c % 10) + '0');
+
+			if (a == 8 && c == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
