48bf5dfd859f9bc3aaff5a7a425d1f0dda12c75d
root <jackmon22523@gmail.com>
2022-09-16 01:45:20 -0700

    isadigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ *isadigit - it checks if number is a digit
+ *@c: character to br tested
+ *Return:1 if is a digit, 0 otherwise
+ */
+
+int_isdigit(int c)
+{
+	if ((c >= 48) && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
