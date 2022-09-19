f4e9ca3226c864c6751d7eecea80449948321611
root <jackmon22523@gmail.com>
2022-09-19 12:07:32 -0700

    strlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *strlen -> function to get length of a string
+ *@s: string pointer to pass this function
+ *Return: returns length of a string
+ */
+int _strlen(char *s)
+	}
+return (strlen(s));
+}
