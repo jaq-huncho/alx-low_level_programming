f20a229eac732ff2f86543aaac523e4659729914
root <jackmon22523@gmail.com>
2022-09-23 12:56:58 -0700

    destin + src

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <string.h>
 /**
- *strcat - concatenates the string pointed to by @src
- *to the end othe stringpointed by @dest
- *@dest: String that will be appended
- *@src: String to concatenated upon
+ * _strcat - concatenates the string pointed to by @src
+ * to the end othe stringpointed by @dest
+ * @dest: String that will be appended
+ * @src: String to concatenated upon
  *
  *Return: returns pointer to @dest
  */
@@ -13,11 +14,10 @@ char *_strcat(char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	while (dest[index])
-		dest_len++;
+	dest_len = strlen(dest);
 
-			for (index = 0; src[index]; index++)
-				dest[dest_len++] = src[index];
+	for (index = 0; src[index] != '\0'; index++)
+		dest[dest_len + index] = src[index];
 
-					return (dest);
+	return (dest);
 }
