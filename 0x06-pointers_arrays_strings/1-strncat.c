225b78f3b0ffbdd5529557bfb3f86edce8cbef71
root <jackmon22523@gmail.com>
2022-09-27 12:54:27 -0700

    two strings concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,39 @@
 #include "main.h"
+
 /**
- * _strncpy - C function that cpoies a string , including the terminating null
- * byte, using at mostan inputted number of bytes.
- * if the length of the strimg is less than themaximum byte number,
- * the remainder of the destination string is filled with null bytes.
- * works identically with standard library 'strncpy'
- * @dest: buffer storing the string copy
- * @src: the source string
- * @n: max number of bytes copied
- * Return: returns
+ * _strncat - concatenate two strings but add inputted number of bytes
+ * @dest: string to be appended upon
+ * @src: string to be completed at end of dest
+ * @n:integer parameter to compare index to
+ * Return: returns new concatenated string
  */
 
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+
+
+	int index = 0, dest_len = 0;
+
+
+
+	while (dest[index++])
+
+		dest_len++;
+
+
+
+	for (index = 0; src[index] && index < n; index++)
+
+		dest[dest_len++] = src[index];
+
+
+
 	return (dest);
+
+
+
 }
