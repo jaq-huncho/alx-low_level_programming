efb29a1ecdf4cc2928e200adfdc2c2a631f23e98
root <jackmon22523@gmail.com>
2022-10-04 16:00:10 -0700

    so static

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <string.h>
+
 /**
- * _strcat - concatenates the string pointed to by @src
- * to the end othe stringpointed by @dest
- * @dest: String that will be appended
- * @src: String to concatenated upon
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
- *Return: returns pointer to @dest
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -14,10 +14,11 @@ char *_strcat(char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	dest_len = strlen(dest);
+	while (dest[index++])
+		dest_len++;
 
-	for (index = 0; src[index] != '\0'; index++)
-		dest[dest_len + index] = src[index];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
